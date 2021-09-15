484a478039cabbfc648c2ac6bdb2f258a43703f9
yasenn <12043095+yasenn@users.noreply.github.com>
2021-09-15 18:04:01 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name  =  'ankiconv',
+    description  =  'Convert SRS cards to the `anki` format',
+    maintainer = 'htpps://github.com/yasenn',
+    maintainer_email = '12043095+yasenn@users.noreply.github.com',
+    version = '0.0.1',
+    include_package_data = True,
+    packages = ['ankiconv'],
+    zip_safe = False
+)
